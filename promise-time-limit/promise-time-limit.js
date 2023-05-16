d9267e1c65a3ef32ddfbb1402ee8a1f2c5b40d33
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-16 09:04:17 +0530

    Time: 56 ms (80.61%) | Memory: 41.4 MB (95.20%) - LeetSync

--- promise-time-limit/promise-time-limit.js
+++ promise-time-limit/promise-time-limit.js
@@ -4,21 +4,12 @@
  * @return {Function}
  */
 var timeLimit = function(fn, t) {
-    return async function(...args) {
-        return new Promise((resolve, reject) => { // create a new promise to return 
-
-        // create a timer to reject the promise if the `t` time passes
-        const timeout = setTimeout(() => {
-            reject("Time Limit Exceeded");
-        }, t);
-
-        // start using the function as usual if it's succed before just return it
-        fn(...args)
-            .then(resolve)
-            .catch(reject)
-            // clear timer is needed to free memory from setTimeout - good practise
-            .finally(() => clearTimeout(timeout));
-        })
+	return async function(...args) {
+        const original = fn(...args);
+        const timeout = new Promise((_, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+        });
+        return Promise.race([original, timeout]);
     }
 };
 
