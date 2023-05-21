7c361eeb27c0a786464b93db82f2c81d16299f3a
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-21 18:23:32 +0530

    Time: 4 ms (83.38%) | Memory: 8.7 MB (6.13%) - LeetSync

--- reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -5,14 +5,16 @@ public:
         string sAns = "";
 
         for(int i = 0; i < s.size(); i++){
-            if(s[i]== 'a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]== 'A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            if(s[i]== 'a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || 
+                    s[i]== 'A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
                 stack.push(s[i]);
             }
         }
 
 
         for(int i = 0; i < s.size(); i++){
-            if(s[i]== 'a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]== 'A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            if(s[i]== 'a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || 
+                    s[i]== 'A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
                 char temp = stack.top();
                 stack.pop();
                 sAns += temp;
