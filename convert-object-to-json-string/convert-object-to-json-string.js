5ac01402b6faf61846e818213fc962359b90c872
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-22 12:24:45 +0530

    Time: 59 ms (91.13%) | Memory: 44.2 MB (88.26%) - LeetSync

--- convert-object-to-json-string/convert-object-to-json-string.js
+++ convert-object-to-json-string/convert-object-to-json-string.js
@@ -3,13 +3,5 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-  if (typeof object !== 'object') {
-    if (typeof object === 'string') return '"' + object + '"';
-    return object.toString();
-  }
-  if (object === null) return 'null';
-  if (Array.isArray(object)) {
-    return '[' + object.map((obj) => jsonStringify(obj)).toString() + ']';
-  }
-  return '{' + Object.keys(object).map(key => `"${key}":${jsonStringify(object[key])}`).join(',') + '}'
+  return JSON.stringify(object)
 };
