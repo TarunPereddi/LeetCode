19d7fd5314d86777d1792ea1a976812943f26505
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-23 13:53:43 +0530

    Time: 180 ms (13.10%) | Memory: 111.7 MB (75.34%) - LeetSync

---
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+    int n = nums.size();
+    int small = INT_MAX;  // Smallest element encountered so far
+    int big = INT_MAX;    // Second smallest element encountered so far
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] <= small) {
+            small = nums[i];  // Update smallest element
+        } else if (nums[i] <= big) {
+            big = nums[i];    // Update second smallest element
+        } else {
+            return true;      // Found a number greater than both small and big
+        }
+    }
+
+    return false;  // No increasing triplet found
+}
+
+};
