d9f51505abdb2af14cba7f17285e896d0072b56f
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-23 11:42:09 +0530

    Time: 482 ms (55.15%) | Memory: 103.1 MB (48.51%) - LeetSync

--- array-of-objects-to-matrix/array-of-objects-to-matrix.js
+++ array-of-objects-to-matrix/array-of-objects-to-matrix.js
@@ -2,47 +2,42 @@
  * @param {Array} arr
  * @return {Matrix}
  */
-
-const jsonToMatrix = function(arr) {
-  // Helper function to check if a value is an object
-  const isObject = x => (x !== null && typeof x === 'object');
-
-  // Recursive function to extract keys from nested objects
-  const getKeys = object => {
-    if (!isObject(object)) return ['']; // If the value is not an object, return an empty key
-    const result = [];
+function flatten(object){
+    const result = []
     for (const key of Object.keys(object)) {
-      const childKeys = getKeys(object[key]); // Recursively get keys from nested objects
-      for (const childKey of childKeys) {
-        result.push(childKey ? `${key}.${childKey}` : key); // Append child keys with dot notation
-      }
+        if (typeof object[key] === 'object' && object[key] !== null) {
+            const flatChild = flatten(object[key]);
+            for (const childKey of Object.keys(flatChild)) {
+                result[`${key}.${childKey}`] = flatChild[childKey];
+            }
+        } else {
+            result[key] = object[key];
+        }
+    }
+
+    return result;
+}
+
+
+
+
+var jsonToMatrix = function(arr) {
+    arr=arr.map(flatten)
+    const keys = new Set(arr.map(Object.keys).flat())
+    const col= [...keys].sort();
+    const result = new Array(arr.length+1).fill().map(_ => new Array(col.length));
+    result[0] = col;
+    for(var i=0; i in arr; i++)
+    {
+        for(var j=0;j in col;j++)
+        {
+            const key=col[j];
+            const value=arr[i][key];
+            if(value === undefined) 
+                result[i+1][j] = '';
+            else
+                result[i+1][j] = value;
+        }
     }
     return result;
-  };
-
-  // Extract all unique keys from the array of objects and sort them
-  const keys = Array.from(new Set(arr.reduce((acc, curr) => {
-    getKeys(curr).forEach(k => acc.add(k));
-    return acc;
-  }, new Set()))).sort();
-
-  // Retrieve the value for a given key path in an object
-  const getValue = (obj, path) => {
-    const paths = path.split('.');
-    let i = 0;
-    let value = obj;
-    while (i < paths.length && isObject(value)) {
-      value = value[paths[i++]];
-    }
-    // If the value is not found or is an object, return an empty string
-    return i < paths.length || isObject(value) || value === undefined ? '' : value;
-  };
-
-  // Create the matrix representation with keys as the first row and values as subsequent rows
-  const matrix = [keys];
-  arr.forEach(obj => {
-    matrix.push(keys.map(key => getValue(obj, key)));
-  });
-
-  return matrix;
 };
