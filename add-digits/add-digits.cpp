a6e93d94aa46dc9252192f856ba9c603fc95a736
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-27 15:56:58 +0530

    Time: 2 ms (53.43%) | Memory: 6.2 MB (9.16%) - LeetSync

--- add-digits/add-digits.cpp
+++ add-digits/add-digits.cpp
@@ -5,8 +5,6 @@ Solution(){
         cin.tie(NULL);   
     }
     int addDigits(int num) {
-        if(num<10) return num;
-        if(num%9 == 0) return 9;
-    return num%9;
+        return (num - 1) % 9 + 1;;
     }
 };
