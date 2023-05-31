571534a1f14e76c30d96b6b94d3f3dac34aaabc3
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-31 07:53:45 +0530

    Time: 59 ms (66.86%) | Memory: 42.9 MB (42.49%) - LeetSync

--- event-emitter/event-emitter.js
+++ event-emitter/event-emitter.js
@@ -1,28 +1,44 @@
 class EventEmitter {
-  constructor() {
-    this.events = {};
-  }
-
-  subscribe(event, cb) {
-    if (!(event in this.events)) {
-      this.events[event] = new Set([cb]);
-    } else {
-      this.events[event].add(cb);
+    constructor() {
+        this.events = new Map();
     }
-
+  subscribe(event, cb) {
+      if (!this.events.has(event)) {
+          this.events.set(event, []);
+      }
+      const listener = this.events.get(event);
+      listener.push(cb);
     return {
-      unsubscribe: () => {
-        this.events[event].delete(cb);
-      },
+        unsubscribe: () => {
+            const index = listener.indexOf(cb);
+            if (index !== -1) {
+            listener.splice(index, 1);
+            }
+        }
     };
   }
 
   emit(event, args = []) {
-    if (!(event in this.events)) return [];
-    const result = [];
-    this.events[event].forEach((fn) => {
-      result.push(fn(...args));
-    });
-    return result;
+      if (this.events.has(event)) {
+          const listener = this.events.get(event);
+          const result = [];
+          for (const l of listener) {
+               result.push(l(...args));
+            }
+          return result;
+      }
+      return [];
   }
 }
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
