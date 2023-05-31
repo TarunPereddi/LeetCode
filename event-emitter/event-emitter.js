eb5d08ccd670cc8a440753475fb9d1ce90912a84
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-05-31 07:55:11 +0530

    Time: 62 ms (51.56%) | Memory: 42.4 MB (72.80%) - LeetSync

--- event-emitter/event-emitter.js
+++ event-emitter/event-emitter.js
@@ -1,31 +1,20 @@
 class EventEmitter {
-  constructor() {
-    this.fns = {};
-  }
-
-  subscribe(e, cb) {
-    this.fns[e] ? this.fns[e].push(cb) : (this.fns[e] = [cb]);
-
+  events = {};
+  subscribe(event, cb) {
+    if (!(event in this.events)) {
+      this.events[event] = [];
+    }
+    this.events[event].push(cb);
+    const idx = this.events[event].length-1
     return {
       unsubscribe: () => {
-        this.fns[e].pop();
+        this.events[event].splice(idx,1);
       },
     };
   }
 
-  emit(e, args = []) {
-    return this.fns[e]?.map((fn) => fn(...args)) || [];
+  emit(event, args = []) {
+    if (!(event in this.events)) return [];
+    return this.events[event].map((fn) => fn(...args));
   }
 }
-
-/**
- * const emitter = new EventEmitter();
- *
- * // Subscribe to the onClick event with onClickCallback
- * function onClickCallback() { return 99 }
- * const sub = emitter.subscribe('onClick', onClickCallback);
- *
- * emitter.emit('onClick'); // [99]
- * sub.unsubscribe(); // undefined
- * emitter.emit('onClick'); // []
- */
