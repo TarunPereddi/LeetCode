733cf32d8f4020da1343b715d1b5999e706af181
Tarun Pereddi <87849811+TarunPereddi@users.noreply.github.com>
2023-06-02 04:19:46 +0530

    Time: 4 ms (42.88%) | Memory: 6.4 MB (5.15%) - LeetSync

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);   
+    }
     double myPow(double x, int n) {
         double X = pow(x,(double)n);
         return X;
